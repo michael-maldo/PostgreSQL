6b34fc6230a22dfcfbc4e235ef61b9293291e97e
gnosticka <gnosticka@gmail.com>
2025-11-29 13:33:37 +1100

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            PostgreSQL Tutorial ⏱️
+            PostgreSQL tutorial ⏱️
           </Link>
         </div>
       </div>
