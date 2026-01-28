d37a9371ba2498ec0754bb5aaaa3c732c948b7b7
gnosticka <gnosticka@gmail.com>
2026-01-28 13:16:42 +1100

    Initial commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,11 +88,11 @@ const config: Config = {
           label: 'Notes',
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
       ],
     },
     footer: {
